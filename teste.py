6bd5191c170b318ff5d29fdad564c73b205bc7be
Code4noobs <41343381+Code4noobs@users.noreply.github.com>
2018-07-17 15:40:39 -0300

    Update teste.py

---
+++ teste.py
@@ -0,0 +1,28 @@
+import requests
+i = str(raw_input("Site: "))
+if i == "":
+	print "Caracteres Nao validos"
+	
+
+i2 = input("Escolha o numero [*]1 para GET[*]2 para POST")
+ 
+if i2 == "":
+ 	print "Numeros nao validos"
+ 	
+elif i2 > 2:
+	print "Numeros nao validos"
+elif i2 < 1:
+	print "numero nao validos"
+
+g = requests.get(i)
+p = requests.post(i)
+s = g.status_code
+s2 = p.status_code
+
+if i2 == 1:
+	print g
+	print "O servidor e", (s)
+if i2 == 2:
+	print p
+	print "O servidor e", (s2)
+
