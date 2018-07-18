2934394c6a4991da3fe42fe16bbc9a5272463c3e
Code4noobs <41343381+Code4noobs@users.noreply.github.com>
2018-07-18 13:59:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,36 +1,4 @@
 import requests
-i = str(raw_input("Site: "))
-if i == "":
-	print "Caracteres Nao validos"
-	
-
-i2 = input("""
-Escolha o numero 
-[*]1 para GET
-[*]2 para POST:")
- 
-if i2 == "":
- 	print "Numeros nao validos"
- 	
-elif i2 > 2:
-	print "Numeros nao validos"
-elif i2 < 1:
-	print "numero nao validos"
-
-g = requests.get(i)
-p = requests.post(i)
-s = g.status_code
-s2 = p.status_code
-
-if i2 == 1:
-	print g
-	print "O servidor deu", (s)
-if i2 == 2:
-	print p
-	print "O servidor deu", (s2)
----------------------
-#OUTRO CODIGO
-import requests
 
 i = raw_input("site:")
 	
@@ -38,15 +6,15 @@ meth = int(raw_input("""Escolha o metodo que deseja:
 	[1] Para GET
 	[2] Para POST
 	[3] Para ver o Codigo Fonte """))
+i2 = requests.get(i)
 if meth == 1:
-	print requests.get(i)
+
+	print i2
 elif meth == 2:
 	print requests.post(i)
 elif meth == 3:
-	i.text
+	print i2.text
 elif meth > 3:
 	print "Numero MAIOR que o pedido"
 elif meth < 1:
 	print "Numero MENOR que o pedido"
-
-
